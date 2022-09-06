088f1106a582ffefeb434316d9e46cf675dc4c55
Henrik Hautakoski <henrik@eossweden.org>
2022-09-06 13:00:40 +0200

    Update search module to handle new package format.

--- assets/package_data.js
+++ assets/package_data.js
@@ -4,13 +4,13 @@
 var package_data = [
     {%- assign repos = site.repos -%}
     {%- for repo in repos -%}
-    {%- assign packages = site.packages | where: 'Repo', repo.slug -%}
+    {%- assign packages = site.packages | where: 'repo', repo.slug -%}
     {%- for pkg in packages -%}
-{"id":{{ id }},"url":{{ pkg.url | relative_url | jsonify }},"name":{{ pkg.Name | jsonify }},"repo":{{ pkg.Repo | jsonify }},"archive":{{ pkg.Archive | jsonify }},"component":{{ pkg.Component | jsonify }},"description": {{ pkg.Description | jsonify }},
+{"id":{{ id }},"url":{{ pkg.url | relative_url | jsonify }},"name":{{ pkg.name | jsonify }},"repo":{{ pkg.repo | jsonify }},"distribution":{{ pkg.distribution | jsonify }},"component":{{ pkg.component | jsonify }},"description": {{ pkg.description | jsonify }},
     {%- assign ver = "-" -%}
-    {%- assign first_ver = pkg.Versions | limit: 1 -%}
+    {%- assign first_ver = pkg.versions | limit: 1 -%}
     {%- for v in first_ver -%}
-        {%- assign ver = v[0] -%}
+        {%- assign ver = v.version -%}
     {%- endfor -%}
 "version":{{ ver | jsonify }}
     {%- assign id = id | plus: 1 -%}
